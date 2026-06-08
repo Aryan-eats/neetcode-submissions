e8323756c96c62690a4e2b7d026a1152ba412cf8
Aryan Kumar <E22cseu1302@bennett.edu.in>
2026-06-08 23:14:33 +0530

    Add: reverse-a-linked-list - submission-0

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    reverseList(head) {
+        let prev = null;
+        let current = head;
+
+        while (current) {
+            const next = current.next;
+
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
