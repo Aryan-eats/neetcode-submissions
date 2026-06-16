9f039ea48911c9695c6ec732fa874399d0911e7c
Aryan Kumar <E22cseu1302@bennett.edu.in>
2026-06-17 02:28:28 +0530

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.js
@@ -0,0 +1,29 @@
+class Solution {
+    evalRPN(tokens) {
+        const stack = [];
+
+        for (const token of tokens) {
+            if (token === "+") {
+                const b = stack.pop();
+                const a = stack.pop();
+                stack.push(a + b);
+            } else if (token === "-") {
+                const b = stack.pop();
+                const a = stack.pop();
+                stack.push(a - b);
+            } else if (token === "*") {
+                const b = stack.pop();
+                const a = stack.pop();
+                stack.push(a * b);
+            } else if (token === "/") {
+                const b = stack.pop();
+                const a = stack.pop();
+                stack.push(Math.trunc(a / b));
+            } else {
+                stack.push(Number(token));
+            }
+        }
+
+        return stack[0];
+    }
+}
